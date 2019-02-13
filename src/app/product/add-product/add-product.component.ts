1de938001a66df199e41953edb8d6b739c66aa67
Vikasini <vikasininithi@gmail.com>
2019-02-13 12:43:38 +0530

    changes in product

--- src/app/product/add-product/add-product.component.ts
+++ src/app/product/add-product/add-product.component.ts
@@ -277,6 +277,7 @@ element.skuCodeVerify = true;
     this.productModel.productDescription = this.productForm.controls.productDescription.value;
     this.productModel.price = this.productForm.controls.price.value;
     this.productModel.color = this.productForm.controls.color.value;
+    this.productModel.mfdQty = this.productForm.controls.mfdQty.value;
     this.productModel.styleCode = this.productForm.controls.styleCode.value.toUpperCase();
     this.productModel.skuCode = this.productForm.controls.skuCode.value.toUpperCase();
     this.productModel.moq = this.moqId;
